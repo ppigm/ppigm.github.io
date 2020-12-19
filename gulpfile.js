71c0e55852a6e951204b4c259001b07c9f02d458
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2020-12-19 21:15:29 +0800

    Update 2020-12-19T13:15:25.707Z

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
-const gulp = require("gulp");
-const gulpLoadPlugins = require("gulp-load-plugins");
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
 
 const $ = gulpLoadPlugins();
 
-gulp.task("publish", () => {
-  console.log("Publish Gitbook (_book) to Github Pages");
-  return gulp.src("./_book/**/*").pipe(
-    $.ghPages({
-      origin: "origin",
-      branch: "gh-pages",
-    })
-  );
+gulp.task('publish', () => {
+  console.log('Publish Gitbook (_book) to Github Pages');
+  return gulp.src('./_book/**/*')
+    .pipe($.ghPages({
+      origin: 'origin',
+      branch: 'gh-pages'
+    }));
 });
---
+++ publish.sh
@@ -0,0 +1,4 @@
+git branch gh-pages
+git checkout gh-pages
+git push -u origin gh-pages
+git checkout master
